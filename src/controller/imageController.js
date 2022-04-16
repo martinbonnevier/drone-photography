c88ba8586bb62fe4fdac060017657203f7337466
Martin Bonnevier <ml225ns@student.lnu.se>
2022-04-16 17:25:32 +0200

    Added logo

--- src/controller/imageController.js
+++ src/controller/imageController.js
@@ -1,20 +1,22 @@
 import { promises } from "fs";
 import fs from "fs";
+let numberOfPages = 0
 let page = 1
 export async function renderIndex(req, res) {
 let images = await loadImages(page)
-res.render('pages/index', {images: images, page:page});  
+res.render('pages/index', {images: images, page:page, numberOfPages: numberOfPages});  
 }
 
 export async function loadImages(currentPage) {
   try {
-    var arr = [];
-    var files = fs.readdirSync("./public/img");
-
-    for (var i = 0; i < files.length; i++) {
+    let arr = [];
+    let files = fs.readdirSync("./public/img");
+    numberOfPages =Math.ceil(files.length/12) 
+    console.log(numberOfPages)
+    for (let i = 0; i < files.length; i++) {
       
       if(i >= (currentPage-1)*12 && i < currentPage*12) {
-        console.log(files[i])
+        // console.log(files[i])
       arr.push(files[i])
       }
     }
